0941d8f574c2c9541036a39cc0288d9c163a757f
klim <klimbox@gmail.com>
2017-12-05 20:18:43 +0200

    add siglalR hub to start page

---
+++ SimpleGame/Scripts/initHub.js
@@ -0,0 +1,17 @@
+﻿$(function () {
+    // Ссылка на автоматически-сгенерированный прокси хаба
+    var game = $.connection.gameHub;
+
+    // Открываем соединение
+    $.connection.hub.start().done(function () {
+        $(document).ready(function () {
+            var name = $("#username").val();
+            if (name) {
+                game.server.connect(name);
+
+                alert('Connected!');
+            }
+        })
+    });
+
+});
